1a1f9739dd69a14e24f3d89aa20c008ac48d53df
Grinja1984@outlook.com <Grigory_Melehov>
2024-10-29 20:36:18 +0300

    hello!

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -6,20 +6,23 @@ driver.get("https://www.saucedemo.com/")
 
 #поиск элементов
 def poisk_elementov():
-    text_pole = driver.find_element(By.CSS_SELECTOR, '#user-name')
-    if text_pole is None:
+
+    text_pole_username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+    if text_pole_username is None:
         print('элемент "поле Username" не найден')
     else:
         print('элемент "поле Username" найден!УРА!')
-    text_pole = driver.find_element(By.CSS_SELECTOR, '#password')
-    if text_pole is None:
+
+    text_pole_password = driver.find_element(By.CSS_SELECTOR, '#password')
+    if text_pole_password is None:
         print('элемент "поле Password" не найден')
     else:
         print('элемент "поле Password" найден!УРА!')
-    text_pole = driver.find_element(By.CSS_SELECTOR, '#login-button') #кнопка называется "Login"
-    if text_pole is None:
-        print('элемент "кнопка Submit" не найден')
+
+    pole_button = driver.find_element(By.CSS_SELECTOR, '#login-button') #кнопка называется "LOGIN"
+    if pole_button is None:
+        print('элемент "кнопка LOGIN" не найден')
     else:
-        print('элемент "кнопка Submit" найден!УРА!')
+        print('элемент "кнопка LOGIN" найден!УРА!')
 
 poisk_elementov()
