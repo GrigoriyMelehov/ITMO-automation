22994b33a37298fafde7ef8fada5b26cba8958b0
Grinja1984@outlook.com <Grigory_Melehov>
2024-10-31 15:47:57 +0300

    hello! modifed

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -5,24 +5,24 @@ driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 
 #поиск элементов
-def poisk_elementov():
+def find_elements():
 
-    text_pole_username = driver.find_element(By.CSS_SELECTOR, '#user-name')
-    if text_pole_username is None:
+    text_element_username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+    if text_element_username is None:
         print('элемент "поле Username" не найден')
     else:
         print('элемент "поле Username" найден!УРА!')
 
-    text_pole_password = driver.find_element(By.CSS_SELECTOR, '#password')
-    if text_pole_password is None:
+    text_element_password = driver.find_element(By.CSS_SELECTOR, '#password')
+    if text_element_password is None:
         print('элемент "поле Password" не найден')
     else:
         print('элемент "поле Password" найден!УРА!')
 
-    pole_button = driver.find_element(By.CSS_SELECTOR, '#login-button') #кнопка называется "LOGIN"
-    if pole_button is None:
+    element_button = driver.find_element(By.CSS_SELECTOR, '#login-button') #кнопка называется "LOGIN"
+    if element_button is None:
         print('элемент "кнопка LOGIN" не найден')
     else:
         print('элемент "кнопка LOGIN" найден!УРА!')
 
-poisk_elementov()
+find_elements()
